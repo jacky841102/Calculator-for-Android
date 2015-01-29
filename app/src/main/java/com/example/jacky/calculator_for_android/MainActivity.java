c6a1534a6c9407f50169dec08b50ff26d6ef9500
Jacky <kuoyichun1102@gmail.com>
2015-01-29 23:44:56 +0800

    Implement basic function, still having bugs

--- app/src/main/java/com/example/jacky/calculator_for_android/MainActivity.java
+++ app/src/main/java/com/example/jacky/calculator_for_android/MainActivity.java
@@ -4,14 +4,86 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private TextView mCalculatorDisplay;
+    private Boolean userIsInTheMiddleOfTypingANumber = false;
+    private CalculatorBrain mCalculatorBrain;
+    private static final String DIGITS = "0123456789.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCalculatorBrain = new CalculatorBrain();
+        mCalculatorDisplay = (TextView) findViewById(R.id.result);
+
+        findViewById(R.id.button0).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        findViewById(R.id.button6).setOnClickListener(this);
+        findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button9).setOnClickListener(this);
+        findViewById(R.id.buttonAddition).setOnClickListener(this);
+        findViewById(R.id.buttonSubtract).setOnClickListener(this);
+        findViewById(R.id.buttonMultiply).setOnClickListener(this);
+        findViewById(R.id.buttonDivide).setOnClickListener(this);
+        findViewById(R.id.buttonClear).setOnClickListener(this);
+        findViewById(R.id.buttonToggleSign).setOnClickListener(this);
+        findViewById(R.id.buttonEquals).setOnClickListener(this);
+        findViewById(R.id.buttonDecimalPoint).setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v){
+        String buttonPressed = ((Button) v).getText().toString();
+        if(DIGITS.contains(buttonPressed)){
+            if (userIsInTheMiddleOfTypingANumber) {
+
+                if (buttonPressed.equals(".") && mCalculatorDisplay.getText().toString().contains(".")) {
+                    // ERROR PREVENTION
+                    // Eliminate entering multiple decimals
+                } else {
+                    mCalculatorDisplay.append(buttonPressed);
+                }
+
+            } else {
+
+                if (buttonPressed.equals(".")) {
+                    // ERROR PREVENTION
+                    // This will avoid error if only the decimal is hit before an operator, by placing a leading zero
+                    // before the decimal
+                    mCalculatorDisplay.setText(0 + buttonPressed);
+                } else {
+                    mCalculatorDisplay.setText(buttonPressed);
+                }
+
+                userIsInTheMiddleOfTypingANumber = true;
+            }
+        }
+        else{
+            if(userIsInTheMiddleOfTypingANumber){
+                mCalculatorBrain.setmOperand(Double.parseDouble(mCalculatorDisplay.getText().toString()));
+                userIsInTheMiddleOfTypingANumber = false;
+            }
+            mCalculatorBrain.performOperation(buttonPressed);
+            mCalculatorDisplay.setText(Double.toString(mCalculatorBrain.getResult()));
+            mCalculatorBrain.setmOperand(0);
+
+        }
+
+
     }
 
 
